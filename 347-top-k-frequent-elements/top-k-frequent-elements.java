7fdd2f674396f2ac34d3e28ce703ed6993980c2a
ptr-18 <146318223+ptr-18@users.noreply.github.com>
2026-04-19 23:19:59 +0530

    Time: 11 ms (96.22%) | Memory: 47.4 MB (83.21%) - LeetSync

---
+++ 347-top-k-frequent-elements/top-k-frequent-elements.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int[] topKFrequent(int[] nums, int k) {
+
+        // Step 1: Frequency map
+        Map<Integer, Integer> frequencyMap = new HashMap<>();
+        for (int n : nums) {
+            frequencyMap.put(n, frequencyMap.getOrDefault(n, 0) + 1);
+        }
+
+        // Step 2: Bucket array
+        List<Integer>[] bucket = new List[nums.length + 1];
+
+        for (int key : frequencyMap.keySet()) {
+            int frequency = frequencyMap.get(key);
+            if (bucket[frequency] == null) {
+                bucket[frequency] = new ArrayList<>();
+            }
+            bucket[frequency].add(key);
+        }
+
+        // Step 3: Collect top k elements
+        int[] res = new int[k];
+        int counter = 0;
+
+        for (int pos = bucket.length - 1; pos >= 0 && counter < k; pos--) {
+            if (bucket[pos] != null) {
+                for (int num : bucket[pos]) {
+                    res[counter++] = num;
+                    if (counter == k) break;
+                }
+            }
+        }
+
+        return res;
+    }
+}
