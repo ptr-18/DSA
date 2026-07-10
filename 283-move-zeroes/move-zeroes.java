83996cf156d407026b612115054c603cbb23ecab
ptr-18 <146318223+ptr-18@users.noreply.github.com>
2026-07-10 15:16:11 +0530

    Time: 2 ms (92.20%) | Memory: 47.7 MB (56.69%) - LeetSync

--- 283-move-zeroes/move-zeroes.java
+++ 283-move-zeroes/move-zeroes.java
@@ -1,13 +1,13 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        int l =0;
-        for(int r =0;r<nums.length;r++){
-            if(nums[r]!=0){
-                int temp =nums[l];
-                nums[l]= nums[r];
-                nums[r]=temp;
-                l++;
-            }
+        int j = 0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]!=0){
+            int temp = nums[i];
+            nums[i]=nums[j];
+            nums[j]=temp;
+            j++;
+        }
         }
         
     }
