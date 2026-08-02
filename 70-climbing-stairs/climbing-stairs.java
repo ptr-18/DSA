cd80b293c9a33bd1f170bbcf492bb45fa940f633
ptr-18 <146318223+ptr-18@users.noreply.github.com>
2026-08-02 22:52:10 +0530

    Time: 0 ms (100.00%) | Memory: 41.8 MB (87.50%) - LeetSync

---
+++ 70-climbing-stairs/climbing-stairs.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int climbStairs(int n) {
+        int[] dp = new int[n + 1];
+        Arrays.fill(dp, -1);
+        return f(n, dp);
+    }
+
+    static int f(int ind, int[] dp) {
+        if (ind == 0)
+            return 1;
+
+        if (ind < 0)
+            return 0;
+
+        if (dp[ind] != -1)
+            return dp[ind];
+
+        int left = f(ind - 1, dp);
+        int right = f(ind - 2, dp);
+
+        return dp[ind] = left + right;
+    }
+}
